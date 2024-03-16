9c3d62ef87fa96620d7d560c94260c299aa05a79
sokunbi yusuf <adeolaoo87@gmail.com>
2024-03-16 10:03:21 +0100

    Add product js and new image to the product

---
+++ data/products.js
@@ -0,0 +1,266 @@
+const products = [{
+  name: 'Shoemaking',
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  rating:
+  {
+    stars: 4.5,
+    count: 87
+  },
+  priceCent: 999
+}, {
+  name: 'Sock',
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  rating:{
+    stars: 5.0,
+    count: 90
+  },
+priceCent: 1500
+},
+{
+  name: 'Bag',
+  image: 'images/products/backpack.jpg',
+  rating:{
+    stars: 4.0,
+    count: 80
+  },
+priceCent: 4550
+},
+{
+  name: 'PLAM',
+  image: 'images/shoe/slippers2.png',
+  rating:{
+    stars: 4.5,
+    count: 85
+  },
+priceCent: 3550
+},
+{
+  name: 'HalfShoe',
+  image: 'images/shoe/halfshoe2.jpg',
+  rating:{
+    stars: 3.0,
+    count: 65
+  },
+priceCent: 4550
+},{
+  name: 'Shoemaking',
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  rating:
+  {
+    stars: 4.5,
+    count: 87
+  },
+  priceCent: 999
+}, {
+  name: 'Sock',
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  rating:{
+    stars: 5.0,
+    count: 90
+  },
+priceCent: 1500
+},
+{
+  name: 'Bag',
+  image: 'images/products/backpack.jpg',
+  rating:{
+    stars: 4.0,
+    count: 80
+  },
+priceCent: 4550
+},
+{
+  name: 'PLAM',
+  image: 'images/shoe/slippers2.png',
+  rating:{
+    stars: 4.5,
+    count: 85
+  },
+priceCent: 3550
+},
+{
+  name: 'HalfShoe',
+  image: 'images/shoe/halfshoe2.jpg',
+  rating:{
+    stars: 3.0,
+    count: 65
+  },
+priceCent: 4550
+},{
+  name: 'Shoemaking',
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  rating:
+  {
+    stars: 4.5,
+    count: 87
+  },
+  priceCent: 999
+}, {
+  name: 'Sock',
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  rating:{
+    stars: 5.0,
+    count: 90
+  },
+priceCent: 1500
+},
+{
+  name: 'Bag',
+  image: 'images/products/backpack.jpg',
+  rating:{
+    stars: 4.0,
+    count: 80
+  },
+priceCent: 4550
+},
+{
+  name: 'PLAM',
+  image: 'images/shoe/slippers2.png',
+  rating:{
+    stars: 4.5,
+    count: 85
+  },
+priceCent: 3550
+},
+{
+  name: 'HalfShoe',
+  image: 'images/shoe/halfshoe2.jpg',
+  rating:{
+    stars: 3.0,
+    count: 65
+  },
+priceCent: 4550
+},{
+  name: 'Shoemaking',
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  rating:
+  {
+    stars: 4.5,
+    count: 87
+  },
+  priceCent: 999
+}, {
+  name: 'Sock',
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  rating:{
+    stars: 5.0,
+    count: 90
+  },
+priceCent: 1500
+},
+{
+  name: 'Bag',
+  image: 'images/products/backpack.jpg',
+  rating:{
+    stars: 4.0,
+    count: 80
+  },
+priceCent: 4550
+},
+{
+  name: 'PLAM',
+  image: 'images/shoe/slippers2.png',
+  rating:{
+    stars: 4.5,
+    count: 85
+  },
+priceCent: 3550
+},
+{
+  name: 'HalfShoe',
+  image: 'images/shoe/halfshoe2.jpg',
+  rating:{
+    stars: 3.0,
+    count: 65
+  },
+priceCent: 4550
+},{
+  name: 'Shoemaking',
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  rating:
+  {
+    stars: 4.5,
+    count: 87
+  },
+  priceCent: 999
+}, {
+  name: 'Sock',
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  rating:{
+    stars: 5.0,
+    count: 90
+  },
+priceCent: 1500
+},
+{
+  name: 'Bag',
+  image: 'images/products/backpack.jpg',
+  rating:{
+    stars: 4.0,
+    count: 80
+  },
+priceCent: 4550
+},
+{
+  name: 'PLAM',
+  image: 'images/shoe/slippers2.png',
+  rating:{
+    stars: 4.5,
+    count: 85
+  },
+priceCent: 3550
+},
+{
+  name: 'HalfShoe',
+  image: 'images/shoe/halfshoe2.jpg',
+  rating:{
+    stars: 3.0,
+    count: 65
+  },
+priceCent: 4550
+},{
+  name: 'Shoemaking',
+  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  rating:
+  {
+    stars: 4.5,
+    count: 87
+  },
+  priceCent: 999
+}, {
+  name: 'Sock',
+  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  rating:{
+    stars: 5.0,
+    count: 90
+  },
+priceCent: 1500
+},
+{
+  name: 'Bag',
+  image: 'images/products/backpack.jpg',
+  rating:{
+    stars: 4.0,
+    count: 80
+  },
+priceCent: 4550
+},
+{
+  name: 'PLAM',
+  image: 'images/shoe/slippers2.png',
+  rating:{
+    stars: 4.5,
+    count: 85
+  },
+priceCent: 3550
+},
+{
+  name: 'HalfShoe',
+  image: 'images/shoe/halfshoe2.jpg',
+  rating:{
+    stars: 3.0,
+    count: 65
+  },
+priceCent: 4550
+}
+];
--- script/amazon.js
+++ script/amazon.js
@@ -1,33 +1,6 @@
 console.log('hello');
 let productHTML = '';
-const products = [{
-  name: 'Shoemaking',
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  rating:
-  {
-    stars: 4.5,
-    count: 87
-  },
-  priceCent: 999
-}, {
-  name: 'Sock',
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  rating:{
-    stars: 5.0,
-    count: 90
-  },
-priceCent: 1500
-},
-{
-  name: 'Bag',
-  image: 'images/products/backpack.jpg',
-  rating:{
-    stars: 4.0,
-    count: 80
-  },
-priceCent: 4550
-}
-];
+
 
 
 

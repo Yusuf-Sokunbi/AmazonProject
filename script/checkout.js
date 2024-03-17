0648814c0fefaa64916ffb3cec6c015d00b56f10
sokunbi yusuf <adeolaoo87@gmail.com>
2024-03-17 13:09:08 +0100

    deletethe product when we click delete

--- script/checkout.js
+++ script/checkout.js
@@ -16,7 +16,8 @@ console.log('out');
   })
   console.log(matchingProduct);
   cartSummaryHTML += `
-  <div class="cart-item-container">
+  <div class="cart-item-container 
+  js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">
     Delivery date: Tuesday, June 21
   </div>
@@ -105,6 +106,9 @@ link.addEventListener('click', () => {
   const productId = link.dataset.productId
   
   removeFromCart(productId)
+
+  const container = document.querySelector(`.js-cart-item-container-${productId}`);
+  container.remove()
   console.log(cart)
 })
 })
